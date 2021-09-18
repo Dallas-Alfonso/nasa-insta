ba7eb316b53537e12e972436a6291e05c804d623
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2021-09-18 17:44:10 -0400

    added firebase

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 
+
 const apiKey = process.env.REACT_APP_NASA_KEY;
 
 export default function NasaPhoto() {
     const [photoData, setPhotoData] = useState(null);
+
+    // // hooks for Likes and Dislikes
+    // const [likes, setlikes] = useState(0);
+    // const [dislikes, setDislikes] = useState(0);
+    // const [movieData, setMovieData] = useState ({}); //ADJUST THIS to pick up NASA DATA
+    // // hooks for Likes and Dislikes
 
     useEffect(() => {
         fetchPhoto();
@@ -22,6 +29,48 @@ export default function NasaPhoto() {
     }, []);
 
     if (!photoData) return <div/>
+
+    // // JS for creating entires in databse, the like  and the dislike 
+    // useEffect(() => {
+    //     if (!url) return null;
+    //     // retrieve likes/dislikes from Firebase and set data
+	// 	// ELSE set a new entry in Firebase with movieID and set data
+    //     const movieRef = firebase.database().ref (`${url}/`);
+    //     movieRef.on("value", (data) => {
+    //         if (data.val() !== null ) {
+    //             setMovieData({
+    //                 ...data.val(),
+    //             });
+    //         } else {
+    //             firebase.database().ref(`${url}/`).set({
+    //                 likes: 0,
+    //                 dislikes: 0,
+    //             });
+    //         }
+    //     })
+    // }, [url]);
+
+    // useEffect(() => {
+    //     if (!movieData) return null;
+    //     const { dislikes: down, likes: up } = movieData;
+
+    //     setDislikes(down);
+    //     setlikes(up);
+    // }, [dislikes, likes, movieData])
+
+    // const handleLikeClick = () => {
+    //     const updates = {};
+    //     updates[`${url}/likes`] = firebase.database.ServerValue.increment(1);
+    //     firebase.database().ref().update(updates);
+    // }
+
+    // const handleDislikeClick = (e) => {
+    //     const updates = {};
+    //     updates[`${url}/dislikes`] = firebase.database.ServerValue.increment(1);
+    //     firebase.database().ref().update(updates)
+    // }
+
+    // // JS for creating entires in databse, the like  and the dislike 
 
 
     return (
@@ -52,6 +101,16 @@ export default function NasaPhoto() {
                 <p className="date">{photoData.date}</p>
                 <p className="explanation">{photoData.explanation}</p>
             </div>
+
+            {/* <div>
+                <button onClick={() => handleLikeClick(imdbID)}>
+                👍{likes}
+                </button>
+                <button onClick={() => handleDislikeClick(imbdID)}>
+                👎{dislikes}
+                </button>
+            </div> */}
+
         </div>
         </>
       
